977a0cf0825da234a5eec677349563377e5b926e
Denis <balance.donika@gmail.com>
2022-12-06 17:44:01 +0600

    HW24.11

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Ввести пятизначное число и определить, палиндром оно или нет.
+
+Console.WriteLine("Введите целое пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 10000 || num > 99999) Console.WriteLine ("Введено неверное значение"); 
+else if (num % 10 == num / 10000) Console.WriteLine("Число является палиндромом");
+else Console.WriteLine("Число не является палиндромом");
+
+
---
+++ Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <=  a; i++)
+{
+    Console.WriteLine($"{i*i*i}");
+}
